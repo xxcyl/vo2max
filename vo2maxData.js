7b33e3b8942dde391e9762b324c83714f23685cb
xxcyl <wittyxx@gmail.com>
2024-08-07 23:24:44 +0800

    Add files via upload

---
+++ vo2maxData.js
@@ -0,0 +1,24 @@
+const vo2maxData = {
+    men: [
+        { age: "20-24", veryPoor: "<32", poor: "32-37", fair: "38-43", average: "44-50", good: "51-56", veryGood: "57-62", excellent: ">62" },
+        { age: "25-29", veryPoor: "<31", poor: "31-35", fair: "36-42", average: "43-48", good: "49-53", veryGood: "54-59", excellent: ">59" },
+        { age: "30-34", veryPoor: "<29", poor: "29-34", fair: "35-40", average: "41-45", good: "46-51", veryGood: "52-56", excellent: ">56" },
+        { age: "35-39", veryPoor: "<28", poor: "28-32", fair: "33-38", average: "39-43", good: "44-48", veryGood: "49-54", excellent: ">54" },
+        { age: "40-44", veryPoor: "<26", poor: "26-31", fair: "32-35", average: "36-41", good: "42-46", veryGood: "47-51", excellent: ">51" },
+        { age: "45-49", veryPoor: "<25", poor: "25-29", fair: "30-34", average: "35-39", good: "40-43", veryGood: "44-48", excellent: ">48" },
+        { age: "50-54", veryPoor: "<24", poor: "24-27", fair: "28-32", average: "33-36", good: "37-41", veryGood: "42-46", excellent: ">46" },
+        { age: "55-59", veryPoor: "<22", poor: "22-26", fair: "27-30", average: "31-34", good: "35-39", veryGood: "40-43", excellent: ">43" },
+        { age: "60-65", veryPoor: "<21", poor: "21-24", fair: "25-28", average: "29-32", good: "33-36", veryGood: "37-40", excellent: ">40" }
+    ],
+    women: [
+        { age: "20-24", veryPoor: "<27", poor: "27-31", fair: "32-36", average: "37-41", good: "42-46", veryGood: "47-51", excellent: ">51" },
+        { age: "25-29", veryPoor: "<26", poor: "26-30", fair: "31-35", average: "36-40", good: "41-44", veryGood: "45-49", excellent: ">49" },
+        { age: "30-34", veryPoor: "<25", poor: "25-29", fair: "30-33", average: "34-37", good: "38-42", veryGood: "43-46", excellent: ">46" },
+        { age: "35-39", veryPoor: "<24", poor: "24-27", fair: "28-31", average: "32-35", good: "36-40", veryGood: "41-44", excellent: ">44" },
+        { age: "40-44", veryPoor: "<22", poor: "22-25", fair: "26-29", average: "30-33", good: "34-37", veryGood: "38-41", excellent: ">41" },
+        { age: "45-49", veryPoor: "<21", poor: "21-23", fair: "24-27", average: "28-31", good: "32-35", veryGood: "36-38", excellent: ">38" },
+        { age: "50-54", veryPoor: "<19", poor: "19-22", fair: "23-25", average: "26-29", good: "30-32", veryGood: "33-36", excellent: ">36" },
+        { age: "55-59", veryPoor: "<18", poor: "18-20", fair: "21-23", average: "24-27", good: "28-30", veryGood: "31-33", excellent: ">33" },
+        { age: "60-65", veryPoor: "<16", poor: "16-18", fair: "19-21", average: "22-24", good: "25-27", veryGood: "28-30", excellent: ">30" }
+    ]
+};
